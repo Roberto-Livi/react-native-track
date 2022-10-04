6cc78dd5cba41bf6fba0899b9d6e4033da58d6b4
Roberto Livi <60152068+Roberto-Livi@users.noreply.github.com>
2022-10-03 23:11:03 -0400

    nodemon for automatic restarts

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+// if you change wifi network -> add new ip address
+const mongoUri = "mongodb+srv://roblivi:roblivi@cluster0.zkpln8x.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
+mongoose.connect(mongoUri);
+
+mongoose.connection.on('connected', () => {
+  console.log("Connected to mongo instance");
+});
+
+mongoose.connection.on('error', () => {
+  console.log("Error connecting to mongo");
+})
 
 app.get('/', (req, res) => {
   res.send('Hi there!');
