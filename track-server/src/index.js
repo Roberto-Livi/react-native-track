f4464611a9a9d8149164e5c3c31a3006cae4f60e
Roberto Livi <60152068+Roberto-Livi@users.noreply.github.com>
2022-10-13 01:14:41 -0400

    using jwt; creating and saving a user

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,10 +1,18 @@
+require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 // if you change wifi network -> add new ip address
 const mongoUri = "mongodb+srv://roblivi:roblivi@cluster0.zkpln8x.mongodb.net/?retryWrites=true&w=majority";
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
+
 mongoose.connect(mongoUri);
 
 mongoose.connection.on('connected', () => {
@@ -15,8 +23,8 @@ mongoose.connection.on('error', () => {
   console.log("Error connecting to mongo");
 })
 
-app.get('/', (req, res) => {
-  res.send('Hi there!');
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3001, () => {
