26b9411efa53aff73f438a4b8f5ed4683ec047a8
Roberto Livi <60152068+Roberto-Livi@users.noreply.github.com>
2022-10-13 21:58:28 -0400

    building a custom express api

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,8 +1,10 @@
 require('./models/User');
+require('./models/Track');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
 const requireAuth = require('./middlewares/requireAuth');
 
 // if you change wifi network -> add new ip address
@@ -12,6 +14,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(authRoutes);
+app.use(trackRoutes);
 
 mongoose.connect(mongoUri);
 
